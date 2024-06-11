16f79d04907c41020f12a1eb0a32e2a884d9bd3f
jiwoos1234 <vmflwlak123@hanmail.net>
2024-06-11 15:57:38 +0900

    init

---
+++ src/p01_class/AnimalEx.java
@@ -0,0 +1,20 @@
+package p01_class;
+
+public class AnimalEx {
+  static Animal a2 = new Animal();
+  // Quiz) Animal을 인스턴스로 만들어 사용해 보시오.
+
+  public  void done() {
+    Animal a1 = new Animal();
+    a1.run();     a2.run();
+  }
+
+  public static void main(String[] args) {
+    Animal a1 = new Animal();
+    a1.run();
+    AnimalEx animalEx = new AnimalEx();
+    animalEx.done();
+    System.out.println(animalEx.a2);
+    //a2.run(); // static 은 같은 static 끼리만 사용: 시점이 다름.
+  }
+}
