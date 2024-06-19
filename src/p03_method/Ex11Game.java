72b66666eb1344db3704392ca669da99306e4f21
jiwoos1234 <vmflwlak123@hanmail.net>
2024-06-19 16:54:47 +0900

    init

--- src/p03_method/Ex11Game.java
+++ src/p03_method/Ex11Game.java
@@ -7,26 +7,25 @@ public class Ex11Game {
 
 
     do {
-      int you = (int) (Math.random() * 3) ;
+      int you = (int) (Math.random() * 3);
       String input = JOptionPane.showInputDialog("가위(0), 바위(1), 보(2)중에 입력하세요");
       int me;
       try {
-         me = Integer.parseInt(input);
+        me = Integer.parseInt(input);
       } catch (Exception e) {
         continue;
       }
-      if ((you - me) == -2 || ( you - me) == 1) {
+      if (me >= 3)
+        System.out.println("다시 입력하세요");
+      else if ((you - me) == -2 || (you - me) == 1) {
         System.out.println("이겼습니다");
-      } else if ((you - me) == -1 || ( you - me) == 2) {
+      } else if ((you - me) == -1 || (you - me) == 2) {
         System.out.println("졌습니다");
-      } else  {
+      } else {
         System.out.println("비겼습니다");
       }
 
-    } while (true)
-
-
-
+    } while (true);
 
 
   }
