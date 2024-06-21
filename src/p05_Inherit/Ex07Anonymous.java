3f7f9ccb30fe52ac047de06628d2263dff9ad2b3
jiwoos1234 <vmflwlak123@hanmail.net>
2024-06-21 16:36:19 +0900

    init

--- src/p05_Inherit/Ex07Anonymous.java
+++ src/p05_Inherit/Ex07Anonymous.java
@@ -1,22 +1,46 @@
-package p05_Inherit;
+package p05_inherit;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ex07Anonymous {
   public static void main(String[] args) {
-    MyButton myButton = new MyButton();
-    Clickable clickable = new Clickable() {
+    Clickable clickable = new Clickable() { // 이름이 없는 객체 -> 익명객체 = anonymous
       @Override
       public void click() {
         System.out.println("click");
       }
     };
+    new MyFrame();
   }
 }
-class MyButton implements Clickable {
+class MyButton implements Clickable{
   @Override
   public void click() {
-    System.out.println("clickMyButton");
+    System.out.println("click My button");
   }
 }
+
 interface Clickable {
   void click();
+}
+
+class MyFrame extends JFrame {
+  public MyFrame() throws HeadlessException {
+    setSize(300,200);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+    setLayout(new FlowLayout());
+    JButton jButton = new JButton("확인");
+    jButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        System.out.println("hello");
+      }
+    });
+    add(jButton);
+    setVisible(true);
+  }
 }
--- src/p05_Inherit/Ex09Abstract.java
+++ src/p05_Inherit/Ex09Abstract.java
@@ -1,38 +1,23 @@
-package p05_Inherit;
+package p05_inherit;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Ex09Abstract {
+public class Ex09Abstarct {
   public static void main(String[] args) {
-  new MyFrame();
+    // 스스로 인스턴스 생성불가(추상 클래스, interface)
+//    Abstract abs = new Abstract();
+//    Interface inter = new Interface();
+
   }
 }
-// 추상클래스 스스로 인스턴스가 안됨.
+// 공통(표준)으로 사용하기 위해 만듦, 인스턴스 생성 불가
 abstract class Abstract {
-  int num = 10; void general(){ }
-  // 추상메서드로 사용할 경우 반드시 abstract 붙일 것
+  int num = 10; void general(){}
+  // 추상 메서드로 사용 할 경우 abstract 반드시 붙일것
   abstract void special();
 }
-interface Interface { }
 
-class MyFrame extends JFrame {
-  public MyFrame() throws HeadlessException {
-    setSize(300, 200);
-    setTitle("My Window");
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setLocationRelativeTo(null);
-    setLayout(new FlowLayout());
-    JButton jButton = new JButton("확인");
-    jButton.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        System.out.println("hello");
-      }
-    });
-    add(jButton);
-    setVisible(true);
-  }
-}
+interface Interface { }
