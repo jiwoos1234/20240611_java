11c2d8e355e7581faf9a9436686ebe6d7b7da278
jiwoos1234 <vmflwlak123@hanmail.net>
2024-06-24 11:46:01 +0900

    init

--- src/p05_Inherit/Ex09Abstract.java
+++ src/p05_Inherit/Ex09Abstract.java
@@ -1,23 +1,21 @@
-package p05_inherit;
+package p05_Inherit;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Ex09Abstarct {
+public class Ex09Abstract {
   public static void main(String[] args) {
-    // 스스로 인스턴스 생성불가(추상 클래스, interface)
+    // 스스로 인스턴스 생성 불가(추상클래스,interface)
 //    Abstract abs = new Abstract();
 //    Interface inter = new Interface();
-
   }
 }
-// 공통(표준)으로 사용하기 위해 만듦, 인스턴스 생성 불가
+// 공통(표준)으로 사용위해 만듦. 인스턴스 생성 불가.
 abstract class Abstract {
-  int num = 10; void general(){}
-  // 추상 메서드로 사용 할 경우 abstract 반드시 붙일것
+  int num = 10; void general(){ }
+  //추상메서드 사용할 경우 반드시 abstract 붙일것
   abstract void special();
 }
-
 interface Interface { }
