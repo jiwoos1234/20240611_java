ce4aa262aba574a61066d42f5a05022d26ba7108
jiwoos1234 <vmflwlak123@hanmail.net>
2024-06-27 08:51:43 +0900

    init

--- src/P08_IO/Ex05Notepad.java
+++ src/P08_IO/Ex05Notepad.java
@@ -4,9 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Ex05Notepad {
   public static void main(String[] args) {
@@ -18,17 +16,23 @@ class Notepad extends JFrame {
   private JMenuBar menuBar;
   private JMenu menuF, menuE, menuO, menuV, menuH;
   private JMenuItem miNew, miOpen, miSave, miExit, miInfo;
-  private JTextArea textArea; private JScrollPane scp;
+  private JTextArea textArea;
+  private JScrollPane scp;
   private JFileChooser fc;
 
   public Notepad() throws HeadlessException {
-    init();  arrange(); inflate();
+    init();
+    arrange();
+    inflate();
   }
+
   //window의 구성품을 초기화
   private void init() {
     menuBar = new JMenuBar();
-    menuF = new JMenu("파일(F)");menuE = new JMenu("편집(E)");
-    menuO = new JMenu("서식(O)");menuV = new JMenu("보기(V)");
+    menuF = new JMenu("파일(F)");
+    menuE = new JMenu("편집(E)");
+    menuO = new JMenu("서식(O)");
+    menuV = new JMenu("보기(V)");
     menuH = new JMenu("도움말(H)");
     miNew = new JMenuItem("새로 만들기(N)");
     miOpen = new JMenuItem("열기(O)");
@@ -54,7 +58,7 @@ class Notepad extends JFrame {
             int data;
             textArea.setText("");
             while ((data = fr.read()) != -1) {
-              textArea.append(String.valueOf((char)data));
+              textArea.append(String.valueOf((char) data));
             }
             fr.close();
           } catch (FileNotFoundException ex) {
@@ -62,23 +66,50 @@ class Notepad extends JFrame {
           } catch (IOException ex) {
             throw new RuntimeException(ex);
           }
+        }
+      }
+    });
 
+    //파일 저장하기
+    miSave.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        int ret = fc.showSaveDialog(miSave);
+        if (ret == 0) {
+          try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fc.getSelectedFile()));
+            writer.write(textArea.getText());
+            writer.close();
+
+          } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+          } catch (IOException ex) {
+            throw new RuntimeException(ex);
+          }
         }
       }
     });
   }
+
   // 배치
   private void arrange() {
-    menuF.add(miNew);menuF.add(miOpen);menuF.add(miSave);
-    menuF.add(miExit); menuH.add(miInfo);
-    menuBar.add(menuF);menuBar.add(menuE);menuBar.add(menuO);
-    menuBar.add(menuV);menuBar.add(menuH);
+    menuF.add(miNew);
+    menuF.add(miOpen);
+    menuF.add(miSave);
+    menuF.add(miExit);
+    menuH.add(miInfo);
+    menuBar.add(menuF);
+    menuBar.add(menuE);
+    menuBar.add(menuO);
+    menuBar.add(menuV);
+    menuBar.add(menuH);
     setJMenuBar(menuBar);
     add(scp);
   }
+
   // 나타나게 함
   private void inflate() {
-    setTitle("찌우's 메모장");
+    setTitle("나의 메모장");
     setSize(500, 300);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLocationRelativeTo(this);
