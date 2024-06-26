b76bc7e83f7ddad89bdd33702ab59aa01444083f
jiwoos1234 <vmflwlak123@hanmail.net>
2024-06-26 11:59:32 +0900

    init

---
+++ src/p07_Collection/Ex09MapApplicate.java
@@ -0,0 +1,28 @@
+package p07_Collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex09MapApplicate {
+  private static Map phoneBook = new HashMap();
+  public static void main(String[] args) {
+    addPhone("친구", "김나연","010-1234-5678");
+    addPhone("친구", "안예지","010-1234-5748");
+    addPhone("친구", "와랄라","010-1234-5778");
+    addPhone("친구", "박지우","010-1234-3218");
+    addPhone("마트","010-1111-5678");
+    printBook(phoneBook);
+  }
+
+  private static void addPhone(String 친구, String 김나연, String s) {
+  }
+  private static void addPhone(String 김나연, String s) {
+  }
+  private static void printBook(HashMap map) {
+
+  }
+
+
+
+
+}
