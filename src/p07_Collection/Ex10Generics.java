7f804b461f01ec7f5931c825af12f8fc3581eba1
jiwoos1234 <vmflwlak123@hanmail.net>
2024-07-01 14:28:26 +0900

    init

--- src/p07_Collection/Ex10Generics.java
+++ src/p07_Collection/Ex10Generics.java
@@ -12,9 +12,11 @@ public class Ex10Generics {
     //2) 타입의 안정성을 도모.
 //    ArrayList list = new ArrayList();  // 제네릭스 미사용
     ArrayList<Card> list = new ArrayList(); //제네릭스 사용
-    list.add(new Card(7,"Spade"));list.add(new Card(2,"Spade"));
-    list.add(new Card(3,"Spade"));
-    list.add(new Card(4,"Spade"));list.add(new Card(5,"Spade"));
+    list.add(new Card(7, "Spade", "rect"));
+    list.add(new Card(2, "Spade", 2));
+    list.add(new Card(3, "Spade", true));
+    list.add(new Card(4, "Spade", new Object()));
+    list.add(new Card(5, "Spade", "A"));
 //    Card card = (Card) list.get(0); // 제네릭스 미사용
     Card card = list.get(0); // 제네릭스 사용
 
@@ -25,7 +27,7 @@ public class Ex10Generics {
     Card[] cards = list.toArray(new IntFunction<Card[]>() { //Java 11 이후
       @Override
       public Card[] apply(int value) {
-        System.out.println(">>"+value); //0인 이유:
+        System.out.println(">>" + value); //0인 이유:
         //배열을 가리키는 참조변수는 null대신 길이가 0인 배열로 초기화하기도 함.
         //배열의 길이가 0인 이유는 배열을 참조하기 위함.
         return new Card[value];
@@ -37,17 +39,38 @@ public class Ex10Generics {
   }
 }
 
-class Card {
-  private int num; private String type;
-  public Card(int num, String type) {this.num = num;this.type=type;}
-  public String toString(){return num + "/" + type;}
+class Card<K, V, G> {
+  private K num;
+  private V type;
+  private G fig;
+
+  public Card(K num, V type, G fig) {
+    this.num = num;
+    this.type = type;
+    this.fig = fig;
+  }
+
+  public void setKVG(K num, V type, G fig) {
+    this.num = num;
+    this.type = type;
+    this.fig = fig;
+  }
+
+  public String toString() {
+    return num + "/" + type;
+  }
 }
+
 class Deck<T> {
   T[] deck;  // Card[] deck;
-  void set(T[] deck) {this.deck = deck;}
+
+  void set(T[] deck) {
+    this.deck = deck;
+  }
+
   void print() {
     for (int i = 0; i < deck.length; i++) {
-      if(i!=0) System.out.print(", ");
+      if (i != 0) System.out.print(", ");
       System.out.print(deck[i]);
     }
     System.out.println();
